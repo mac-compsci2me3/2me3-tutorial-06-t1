44d83142843bf8cda384f673b09e1ed6f502a5c9
DFPOV <lichuchen77@gmail.com>
2024-10-28 11:21:07 -0400

    wawdaawd

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,10 @@ public class Client {
         // Create some shapes with specified color
         Shape square = new Square("Red", 5.0);
         Shape circle = new Circle("Blue", 3.0);
+        Drawing drawing = Drawing.getInstance();
+
+        drawing.DrawShape(circle);
+        drawing.DrawShape(square);
 
         // Uncomment the following after implementing your drawing class
         // // Retrieve the stance of drawing
--- src/Drawing.java
+++ src/Drawing.java
@@ -1,3 +1,18 @@
 public class Drawing {
-    
+    private static Drawing instance;
+
+    public Drawing(){
+    }
+
+    public void DrawShape(Shape shape){
+        System.out.println("Drawing a " + shape.getClass().getSimpleName() + " with color: " + shape.getColor() + " and area: " + shape.calculateArea());
+    }
+
+    public static Drawing getInstance(){
+        if (instance == null){
+            instance = new Drawing();
+        }
+
+        return instance;
+    }
 }
